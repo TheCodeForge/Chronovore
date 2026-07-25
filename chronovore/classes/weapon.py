733801788724a1ac7aad9fc472fdb8ac907712a5
CodeForge <92867553+TheCodeForge@users.noreply.github.com>
2026-07-25 10:17:36 -0400

    Revert "Update weapon.py"
    This reverts commit aa27be432a7c7800fcdec3cc517dc765c9cfbab9.

--- chronovore/classes/weapon.py
+++ chronovore/classes/weapon.py
@@ -10,7 +10,7 @@ from chronovore.__main__ import cache
 class Weapon(Base):
 
     def __repr__(self):
-        return f"<Weapon({self.faction.name} / {self.name})>"
+        return f"<Weapon({self.faction.name} / {self.id})>"
 
     @property
     @cache.memoize()
