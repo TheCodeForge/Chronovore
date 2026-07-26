a71b4eb25a5c36f46e84b2b79e0a9ba1371d6264
CodeForge <92867553+TheCodeForge@users.noreply.github.com>
2026-07-26 17:53:08 -0400

    Update weapon.py

--- chronovore/classes/weapon.py
+++ chronovore/classes/weapon.py
@@ -29,65 +29,90 @@ class Weapon(Base):
         if self.profiles:
             return max([x.weapon_points_raw for x in self.profiles])
 
-        if not self.__dict__.get('str'):
-            return 0
-
-        if isinstance(self.dmg, str):
-            dmg = eval(self.dmg.replace("2d6", "7").replace("d6", "3.5").replace("d3", "2"))
-        else:
-            dmg = self.dmg
-
+        #evaluate attack and damage strings, as needed
         if isinstance(self.atk, str):
             atk = eval(self.atk.replace("2d6", "7").replace("d6", "3.5").replace("d3", "2"))
         else:
             atk = self.atk
 
-        if "Torrent" in self.keywords:
-            skl=1
+        if isinstance(self.dmg, str):
+            dmg = eval(self.dmg.replace("2d6", "7").replace("d6", "3.5").replace("d3", "2"))
+        else:  
+            dmg = self.dmg
+
+        if "psk" in self.___dict__:
+
+            rng = self.__dict__.get('rng')
+
+            psk_map={
+                2:36,
+                3:35,
+                4:33,
+                5:30,
+                6:26,
+                7:21,
+                8:15,
+                9:10,
+                10:6,
+                11:3,
+                12:1,
+            }
+
+            weapon_pts = psk_map[self.psk] * self.atk * math.sqrt(self.ap+1) + dmg * math.sqrt(rng)
+
+
+
         else:
-            skl = self.skl
 
-        rng = self.__dict__.get('rng',7) #7 is average effective range of melee weapons due to charge
+            if not self.__dict__.get('str'):
+                return 0
 
-        weapon_pts = (atk * (7-skl) * sigmoid(self.str) * math.sqrt(self.ap+1) * dmg * math.sqrt(rng))
+            if "Torrent" in self.keywords:
+                skl=1
+            else:
+                skl = self.skl
 
-        for kwd in self.keywords:
-            if kwd.startswith("Anti-"):
-                weapon_pts *= 1.1
-            elif kwd=="Assault":
-                weapon_pts *= 1.1
-            elif kwd=="Blast":
-                weapon_pts*=1.1
-            elif kwd=="Devastating Wounds":
-                weapon_pts *= 1.2
-            elif kwd=="Hazardous":
-                weapon_pts *= 5/6
-            elif kwd=="Heavy":
-                weapon_pts *= 1.1
-            elif kwd=="Ignore Cover":
-                weapon_pts *= 1.2
-            elif kwd=="Incendiary":
-                weapon_pts *= 1.1
-            elif kwd=="Indirect Fire":
-                weapon_pts *= 1.1
-            elif kwd=="Lance":
-                weapon_pts *= 1.1
-            elif kwd=="Lethal Hits":
-                weapon_pts *= 1.2
-            elif kwd.startswith("Melta"):
-                weapon_pts *= 1.2
-            elif kwd=="One-Shot":
-                weapon_pts /= 5
-            elif kwd=="Pistol":
-                weapon_pts *= 1.1
-            elif kwd=="Precise":
-                weapon_pts *= 1.1
-            elif kwd.startswith("Rapid Fire"):
-                weapon_pts *= 1.5
-            elif kwd.startswith("Sustained Hits"):
-                weapon_pts *= 1.17
-            elif kwd=="Twin-Linked":
-                weapon_pts *= 1.25
+            rng = self.__dict__.get('rng',7) #7 is average effective range of melee weapons due to charge
+
+            weapon_pts = (atk * (7-skl) * sigmoid(self.str) * math.sqrt(self.ap+1) * dmg * math.sqrt(rng))
+
+            for kwd in self.keywords:
+                if kwd.startswith("Anti-"):
+                    weapon_pts *= 1.1
+                elif kwd=="Assault":
+                    weapon_pts *= 1.1
+                elif kwd=="Blast":
+                    weapon_pts*=1.1
+                elif kwd=="Devastating Wounds":
+                    weapon_pts *= 1.2
+                elif kwd=="Hazardous":
+                    weapon_pts *= 5/6
+                elif kwd=="Heavy":
+                    weapon_pts *= 1.1
+                elif kwd=="Ignore Cover":
+                    weapon_pts *= 1.2
+                elif kwd=="Incendiary":
+                    weapon_pts *= 1.1
+                elif kwd=="Indirect Fire":
+                    weapon_pts *= 1.1
+                elif kwd=="Lance":
+                    weapon_pts *= 1.1
+                elif kwd=="Lethal Hits":
+                    weapon_pts *= 1.2
+                elif kwd.startswith("Melta"):
+                    weapon_pts *= 1.2
+                elif kwd=="One-Shot":
+                    weapon_pts /= 5
+                elif kwd=="Pistol":
+                    weapon_pts *= 1.1
+                elif kwd=="Precise":
+                    weapon_pts *= 1.1
+                elif kwd.startswith("Rapid Fire"):
+                    weapon_pts *= 1.5
+                elif kwd.startswith("Sustained Hits"):
+                    weapon_pts *= 1.17
+                elif kwd=="Twin-Linked":
+                    weapon_pts *= 1.25
 
         # print(f"    {self.name}: {weapon_pts}")
         return int(weapon_pts)
