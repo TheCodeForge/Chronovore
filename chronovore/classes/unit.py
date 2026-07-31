b93d17c922fef6d08c815e35461eb53ed29092df
CodeForge <92867553+TheCodeForge@users.noreply.github.com>
2026-07-31 17:33:23 -0400

    Update unit.py

--- chronovore/classes/unit.py
+++ chronovore/classes/unit.py
@@ -96,7 +96,7 @@ class Unit(Base):
         psychic_offensive = max([x.weapon_points_raw for x in self.psychic_weapons if x.id in self.default_gear] or [0])
 
         if self.psk and psychic_offensive:
-            psychic_difficulty = max([x.psk for x in self.psychic_weapons if x.id in self.default_gear])
+            psychic_difficulty = min([x.psk for x in self.psychic_weapons if x.id in self.default_gear] or [0])
             true_difficulty = max(psychic_difficulty-self.psk, 2)
             psychic_offensive *= dice_map[true_difficulty]/dice_map[psychic_difficulty] 
 
