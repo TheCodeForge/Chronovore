b95f37f81fd66848bb025ffb5f34a660a456b8ec
CodeForge <92867553+TheCodeForge@users.noreply.github.com>
2026-08-07 17:57:35 -0400

    Update data.py

--- chronovore/routes/data.py
+++ chronovore/routes/data.py
@@ -55,10 +55,8 @@ def faction_faction_detachment_detachment_armylist(faction, detachment):
 
     units = []
 
-    print(f.unit_listing)
-
     for role in f.unit_listing:
-        for unit in role:
+        for unit in f.unit_listing[role]:
             if session.get(f'qty_{f.id}_{unit.id}', 0):
                 units.append(unit)
 
